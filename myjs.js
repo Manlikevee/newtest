1758ab720d718ac3e56d99f3ae8533346618840f
Manlikevee <90337435+Manlikevee@users.noreply.github.com>
2022-04-29 22:18:07 +0100

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,18 @@
+let body = document.querySelector("body"),
+    lightBox = document.querySelector(".lightBox"),
+    img = document.querySelectorAll(".gImg"),
+    showImg = lightBox.querySelector(".showImg img"),
+    name = lightBox.querySelector(".name")
+    close = lightBox .querySelector(".close");
+
+   for (let image of img) {
+     image.addEventListener("click", ()=>{
+       showImg.src = image.src;
+       lightBox.style.display = "block";
+       body.style.overflow = "hidden";
+       close.onclick = ()=>{
+         lightBox.style.display = "none";
+         body.style.overflow = "visible";
+       };
+     });
+   }
